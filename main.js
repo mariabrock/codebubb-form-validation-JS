c8a8cc2db9da33b7b2a58388c366f60cde3f101d
Maria Brock <brockm@ornl.giv>
2023-09-27 15:46:20 -0400

    its mostly working

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const validateForm = formSelector => {
+const validateForm = (formSelector, callback) => {
 	const formElement = document.querySelector(formSelector);
 
 	// we will create validation rules that the group can loop through to see if ever ything validates
@@ -9,7 +9,7 @@ const validateForm = formSelector => {
 				input.value &&
 				input.value.length >= parseInt(input.minLength, 10),
 			errorMessage: (input, label) =>
-				`${ label.textContent } needs to be at least ${input.minLength} characters`
+				`${ label.textContent } needs to be at least ${ input.minLength } characters`
 		},
 		// doesn't work, not sure why, will explore later
 		// {
@@ -21,45 +21,46 @@ const validateForm = formSelector => {
 		// 		`${ label.textContent } needs to be less than ${input.getAttribute('custommaxlength')} characters`
 		// },
 		{
-			attribute: 'match',
-			isValid: input => {
-				const matchSelector = input.getAttribute('match');
-				const matchedElement = formElement.querySelector(`#${matchSelector}`);
-				return matchedElement && matchedElement.value.trim() === input.value.trim();
-			},
-			errorMessage: (input, label) => {
-				// reminder: check how many parent elements there are when trying to grab elements!
-				const matchSelector = input.getAttribute('match');
-				const matchedElement = formElement.querySelector(`#${matchSelector}`);
-				const matchedLabel =
-					matchedElement.parentElement.querySelector('label');
- 				return `${label.textContent} should match ${matchedLabel.textContent}`;
-			}
-		},
-		{
 			attribute: 'pattern',
 			isValid: input => {
 				const patternRegex = new RegExp(input.pattern);
 				return patternRegex.test(input.value);
 			},
-			errorMessage: (input, label) => `Not a valid ${label.textContent}`
+			errorMessage: (input, label) => `Not a valid ${ label.textContent }`
+		},
+		{
+			attribute: 'match',
+			isValid: input => {
+				const matchSelector = input.getAttribute('match');
+				const matchedElement = formElement.querySelector(`#${ matchSelector }`);
+				return matchedElement && matchedElement.value.trim() === input.value.trim();
+			},
+			errorMessage: (input, label) => {
+				// reminder: check how many parent elements there are when trying to grab elements!
+				const matchSelector = input.getAttribute('match');
+				const matchedElement = formElement.querySelector(`#${ matchSelector }`);
+				const matchedLabel =
+					matchedElement.parentElement.querySelector('label');
+				return `${ label.textContent } should match ${ matchedLabel.textContent }`;
+			}
 		},
 		{
 			attribute: 'required',
 			isValid: input => input.value.trim() !== '',
-			errorMessage: (input, label) => `${label.textContent} is required`
+			errorMessage: (input, label) => `${ label.textContent } is required`
 
 		},
 	];
 
 	const validateSingleFormGroup = formGroup => {
 		const label = formGroup.querySelector('label');
-		const input = formGroup.querySelector('input');
+		const input = formGroup.querySelector('input'); // also textarea
 		const errorContainer = formGroup.querySelector('.error');
 		const errorIcon = formGroup.querySelector('.error-icon');
 
 		let formGroupError = false;
-		// loop through our validation options
+		// loop through our validation options and add classes to show errors
+		// set formGroupError to true
 		for(const option of validationOptions) {
 			if(input.hasAttribute(option.attribute) && !option.isValid(input)) {
 				errorContainer.textContent = option.errorMessage(input, label);
@@ -68,42 +69,62 @@ const validateForm = formSelector => {
 				formGroupError = true;
 			}
 		}
-		// another statement to set error containers to nothing
+		// sets error containers to nothing when there is no error
 		if(!formGroupError) {
 			errorContainer.textContent = '';
 			input.classList.remove('error-input');
 			errorIcon.classList.add('hidden');
 		}
+		// get a true value if there are no formGroup errors
+		return !formGroupError;
 	};
 
-// novalidate removes all html validation so that we can start with a clean slate
+	// novalidate removes all browser html validation so that we can start with a clean slate
 	formElement.setAttribute('novalidate', '');
+
+	// function to validate each form group we created
+	const validateAllFormGroups = formToValidate => {
+		// grab form groups out of the form, store in an array
+		const formGroups = Array.from(formToValidate.querySelectorAll('.form-group'));
+		// since validateSingleFormGroup now returns a boolean we will use every instead of forEach
+		// this checks that all groups return a true value
+		// every is an array func that loops through and calls any function we want,
+		// depending on whether it gets a true or false value will determine what it returns
+		return formGroups.every(formGroup => validateSingleFormGroup(formGroup));
+	};
 
 	// in order to listen for validation while user is filling in the form, we'll write a function to listen on 'blur'
 	// which is a function triggered each time the user moves to a new form element
 	// loop through each input, listen for blur/keydown/update grab event object, and validate
-	Array.from(formElement.elements).forEach(element => {
+	Array.from(formElement.elements).forEach(element =>
 		element.addEventListener('blur', event => {
 			validateSingleFormGroup(event.target.parentElement);
 		})
-	});
+	);
 
-// add an event listener so we can prevent automatic submission
-// 	trigger the validation
+	// add an event listener to we can prevent automatic submission
+	// 	trigger the validation
 	formElement.addEventListener('submit', (event) => {
-		event.preventDefault();
-		validateAllFormGroups(formElement);
+		console.log('test');
+		const formValid = validateAllFormGroups(formElement);
+
+		if(!formValid) {
+			event.preventDefault();
+		} else {
+			console.log('Form is valid');
+			callback(formElement);
+		}
 	});
-// function to validate each form group we created
-	const validateAllFormGroups = formToValidate => {
-		// grab form groups out of the form, store in an array
-		const formGroups = Array.from (formToValidate.querySelectorAll('.form-group'));
-
-		formGroups.forEach( formGroup => {
-			validateSingleFormGroup(formGroup)
-		});
-	}
-
 }
 
-validateForm('#registrationForm')
+	const sendToAPI = formElement => {
+		const formObject = Array.from(formElement.elements)
+			.filter(element => element.type !== 'submit')
+			.reduce((accumulator, element) => ({
+				...accumulator, [element.id]: element.value
+			}), {});
+		console.log(formObject)
+		// submit to and API via Ajax or other
+	}
+
+validateForm('#registrationForm', sendToAPI);
